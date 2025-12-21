dc869fe22af20fa4fcd1ac0d72c7153c2f15fc26
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-21 11:10:35 +0200

    Add express files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', userRoutes);
+
+app.get('/', (req, res) => {
+  res.json({ info: 'Node.js, Express, and Postgres API Template by Sonick Mumba' });
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
