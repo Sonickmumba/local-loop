3983c0890aac46377b47689221bad7b09fbdd08e
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-21 13:45:22 +0200

    Add database name in the .env file

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config;
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',,
+  user: process.env.DB_USER || 'postgres',
   host: 'localhost',
   database: process.env.DB_NAME || 'localloop',
   password: process.env.DB_PASSWORD || '',
