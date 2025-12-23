352f0f331a9ab6dd277f34446bba47d35744d1f1
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-23 18:15:51 +0200

    Update notificationsController.js

--- controllers/notificationsController.js
+++ controllers/notificationsController.js
@@ -1,9 +1,35 @@
 const pool = require('../config/db');
 
 
-exports.getNotifications = async (req, res, next) => {
+exports.getUserNotifications = async (req, res, next) => {
     try {
+        const userId = req.user.userId;
+        const { unreadOnly } = req.query;
+
+        let query = `SELECT * FROM notifications WHERE user_id = $1`;
+        const params = [userId];
+
+        if (unreadOnly === 'true') {
+            query += ` AND is_read = false`;
+        }
+
+        query += ` ORDER BY created_at DESC LIMIT 50`;
+
+        const notificationsResult = await pool.query(query, params);
+
+        const { rows } = await pool.query(query, params);
+
         
+        const notifications = rows.map(notif => ({
+            ...notif,
+            timeAgo: timeAgo(notif.created_at)
+        }));
+
+        res.json({
+            success: true,
+            count: notifications.length,
+            data: notifications
+        });
     } catch (error) {
         next(error);
     }
