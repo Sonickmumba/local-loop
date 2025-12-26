a6313a719e0427c0d134a1c4690584b494f6f5ce
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-26 16:24:23 +0200

    Remove default files from the the install react

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -4,13 +4,14 @@
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <h1 className="text-5xl font-bold underline">Hello world!</h1>
       </div>
+      
+      
     </>
   );
 }
