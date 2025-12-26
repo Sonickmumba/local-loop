2745f733f1b0454c2c1dfaf34e2f40b3a22c6d64
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-26 20:14:28 +0200

    Update App.jsx

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -7,18 +7,23 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import { WelcomeScreen } from './components/WelcomeScreen';
+import { WelcomeScreen } from './features/welcome/WelcomeScreen';
+import { LocationPermissionScreen } from './features/location/LocationPermissionScreen';
 
 import './App.css';
 
 function App() {
+  const { permission, coords } = useSelector((s) => s.location);
+  console.log('Location state in App:', { permission, coords });
   return (
     <Router>
       <div className="min-h-screen bg-white">
         <Routes>
           <Route path="/" element={<Navigate to="/welcome" replace />} />
           <Route path="/welcome" element={<WelcomeScreen />} />
+          <Route path="/location" element={<LocationPermissionScreen/>} />
         </Routes>
       </div>
     </Router>
