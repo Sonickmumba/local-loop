0e47c12352c8045ebf93ace747fcf58df96cc053
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-30 19:50:19 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -21,5 +21,8 @@ router.post('/login', [
 // get currrent user
 router.get('/me', authMiddleware, authController.getCurrentUser);
 
+router.post('/logout', authController.logout);
+
+
 
 module.exports = router;
