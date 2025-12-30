bb06b68d61f3beae7ed704e7bf16459a352a04df
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-30 11:35:22 +0200

    Update store.js

--- views/src/app/store.js
+++ views/src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import locationReducer from '../features/location/locationPermissionSlice';
 import authReducer from '../features/auth/authSlice';
+import homeFeedReducer from '../features/home/homeFeedSlice';
 
 const store = configureStore({
   reducer: {
     location: locationReducer,
     auth: authReducer,
+    homeFeed: homeFeedReducer,
   },
 });
 
