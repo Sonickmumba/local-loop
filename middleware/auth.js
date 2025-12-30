3caf7834c96abaed6de0d5acf6084f126e873b4f
Sonick Mumba <kingellie.mumba@gmail.com>
2025-12-30 19:50:22 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
   try {
     // Get token from header
-    const token = req.headers.authorization?.split(' ')[1]; // Bearer TOKEN
+    // const token = req.headers.authorization?.split(' ')[1]; // Bearer TOKEN
+    const token = req.cookies.access_token;
 
     if (!token) {
       return res.status(401).json({
@@ -29,5 +30,27 @@ const authMiddleware = (req, res, next) => {
     });
   }
 };
+
+
+
+
+// const jwt = require('jsonwebtoken');
+
+// module.exports = (req, res, next) => {
+//   const token = req.cookies.access_token;
+
+//   if (!token) {
+//     return res.status(401).json({ message: 'Not authenticated' });
+//   }
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch {
+//     return res.status(401).json({ message: 'Invalid token' });
+//   }
+// };
+
 
 module.exports = authMiddleware;
