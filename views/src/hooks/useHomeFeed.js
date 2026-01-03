195325ab02871a7963f27670bc27bdd8189175ff
Sonick Mumba <kingellie.mumba@gmail.com>
2026-01-03 22:19:43 +0200

    Create useHomeFeed.js

---
+++ views/src/hooks/useHomeFeed.js
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchHomeFeed } from '../features/home/homeFeedSlice';
+
+export const useHomeFeed = () => {
+  const dispatch = useDispatch();
+  const { listings, status, error } = useSelector((s) => s.homeFeed);
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchHomeFeed());
+    }
+  }, [status, dispatch]);
+
+  return { listings, status, error, refetch: () => dispatch(fetchHomeFeed()) };
+};
