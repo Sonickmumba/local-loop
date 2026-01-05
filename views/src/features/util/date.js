6282eb6627a2e17d1dbec34d8c7cd6dd0fe47630
Sonick Mumba <kingellie.mumba@gmail.com>
2026-01-05 23:29:21 +0200

    Update date.js

--- views/src/features/util/date.js
+++ views/src/features/util/date.js
@@ -3,3 +3,27 @@ export const formatMonthYear = (date) =>
     month: 'long',
     year: 'numeric',
   });
+
+  export const formatTradeDate = (isoString) => {
+  const date = new Date(isoString);
+
+  const day = date.toLocaleDateString('en-US', {
+    weekday: 'long'
+  });
+
+  const month = date.toLocaleDateString('en-US', {
+    month: 'short'
+  });
+
+  const dayOfMonth = date.getDate();
+
+  const year = date.getFullYear();
+
+  return {
+    day,        // Saturday
+    month,      // Dec
+    date: dayOfMonth, // 28
+    year        // 2026
+  };
+}
+
