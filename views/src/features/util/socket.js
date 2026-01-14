9309a3074b665ef21a832a3711a16654e6ab412b
Sonick Mumba <kingellie.mumba@gmail.com>
2026-01-14 15:16:46 +0200

    Update socket.js

--- views/src/features/util/socket.js
+++ views/src/features/util/socket.js
@@ -4,7 +4,7 @@ const SOCKET_SERVER_URL = 'http://localhost:3000';
 
 const socket = io(SOCKET_SERVER_URL, {
   withCredentials: true,
-  autoConnect: false, // VERY IMPORTANT
+  autoConnect: true, // VERY IMPORTANT
 });
 
 export default socket;
