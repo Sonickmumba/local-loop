512cf27809bd907dd1359049406e84f23e719d82
Sonick Mumba <kingellie.mumba@gmail.com>
2026-01-19 06:24:59 +0200

    Update homeFeedSlice.js

--- views/src/features/home/homeFeedSlice.js
+++ views/src/features/home/homeFeedSlice.js
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { createListing } from '../listings/listingsSlice';
-// import { useSelector } from 'react-redux';
 
 export const fetchHomeFeed = createAsyncThunk(
   'homeFeed/fetchHomeFeed',
@@ -9,14 +8,9 @@ export const fetchHomeFeed = createAsyncThunk(
     try {
       // const user = useSelector((s) => s.auth.user);
       const res = await axios.get('http://localhost:3000/api/listings/', {
-        // params: {
-        //   lat: user?.location_lat,
-        //   lng: user?.location_lng,
-        // },
         withCredentials: true,
       });
 
-      console.log(res.data.data);
       return res.data.data;
     } catch (err) {
       if (err.response?.status === 401) {
