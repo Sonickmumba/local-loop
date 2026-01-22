282d7e648b5d356d3723e2d8c9876f78dc92b131
Sonick Mumba <kingellie.mumba@gmail.com>
2026-01-22 23:02:03 +0200

    Update App.jsx

--- views/src/App.jsx
+++ views/src/App.jsx
@@ -31,7 +31,6 @@ import './App.css';
 import { NotificationsScreen } from './features/notification/NotificationsScreen.jsx';
 import { TradeNegotiationScreen } from './features/trade/TradeNegotiatonScreen.jsx';
 import { TradeManagementScreen } from './features/trade/TradeManagementScreen.jsx';
-// import { ReviewRatingScreen } from './features/review/reviewRatingScreen.jsx';
 import { ReviewRating } from './features/review/ReviewRating';
 import { Toaster } from 'react-hot-toast';
 
@@ -39,10 +38,8 @@ import { Toaster } from 'react-hot-toast';
 function App() {
   const dispatch = useDispatch();
   const initialized = useSelector((s) => s.auth.initialized);
-
-  // Location permission and coordinates from Redux store
+  
   const { coords } = useSelector((s) => s.location);
-  console.log(coords)
 
   useEffect(() => {
     dispatch(bootstrapSession());
